76426f0f0bd533889e01c4d158f15f2167a29715
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-24 22:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // write js code here if required
+
+const timerDisplay=document.getElementById("timer");
+
+setInterval(()=>{
+
+	let info=new Date();
+	let date = info.getDate();
+	let month=info.getMonth();
+	let year = info.getFullYear();
+	let h=info.getHours();
+	let m= info.getMinutes();
+	let s = info.getSeconds();
+	if(s<10)
+	{
+		s="0"+s;
+	}
+	
+	timerDisplay.innerText=date + "/" +month+ "/" +year + "," + h +":"+m+":"+s ;
+} , 1000);
